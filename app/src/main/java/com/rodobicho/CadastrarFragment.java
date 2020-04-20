28cafb513afe490e857b1cafd9e0568f43cf08d8
Giulliana <qlcimz@gmail.com>
2020-04-20 20:16:32 -0300

    Add - cadastrar & fix - acesso a cadastrar pessoa fragment

---
+++ app/src/main/java/com/rodobicho/CadastrarFragment.java
@@ -0,0 +1,30 @@
+package com.rodobicho;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CadastrarFragment extends Fragment {
+
+    public CadastrarFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+       View view = inflater.inflate(R.layout.activity_cadastrar, container, false);
+
+       return view;
+
+
+    }
+}
+
+
+
