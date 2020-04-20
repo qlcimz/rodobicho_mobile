818df3430b2ad0875223e7fe153b5f96a9eb9a07
Giulliana <qlcimz@gmail.com>
2020-04-20 19:21:39 -0300

    Update - link dados para cadastrar[fragment]

--- app/src/main/java/com/rodobicho/DadosFragment.java
+++ app/src/main/java/com/rodobicho/DadosFragment.java
@@ -4,16 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class DadosFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_dados, container, false);
+
+
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            View view = inflater.inflate(R.layout.activity_dados, container, false);
+
+            Button btnCadastrar = (Button) view.findViewById(R.id.btn_cadastrar);
+            btnCadastrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FragmentTransaction fr =getFragmentManager().beginTransaction();
+                    fr.replace(R.id.fragment_container, new CadastrarFragment());
+                    fr.commit();
+                }
+            });
+
+
+
+            return view;
+
+        }
     }
-}
+
